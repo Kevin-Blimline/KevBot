ecc3061ff3af3223e1354a90875084aa7e618b04
Kevin Blimline <kblimy2@gmail.com>
2020-05-13 22:06:03 -0400

    Test1

--- kevbot.py
+++ kevbot.py
@@ -7,7 +7,7 @@ client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
-    await client.change_presence(status = discord.Status.online, activity = discord.Game('captcha simulator'))
+    await client.change_presence(status = discord.Status.online, activity = discord.Game('captcha simulator - Test'))
     print('Bot is online')
 
 @client.command(aliases = ['game', 'RanGame','randomgame','rangame'], brief='Type in games seperated by commas')
