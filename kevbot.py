e8ae9e115f886cd62a86da0645970db4ca675212
Kevin-Blimline <64049429+Kevin-Blimline@users.noreply.github.com>
2022-01-21 19:30:41 -0500

    Update kevbot.py

--- kevbot.py
+++ kevbot.py
@@ -23,8 +23,8 @@ async def RandomGame(ctx, *, games):
 async def WheresMatt(ctx):
     #member = discord.utils.get(ctx.message.guild.members, name='GoatMilk')
     #member = discord.utils.get(client.users, name="GoatMilk", discriminator="8908")
-    member = "304791011635494913"
-    await message.channel.send(f"<@{member}> is on the couch")
+    user_id = "304791011635494913"
+    await message.channel.send(f"<@{user_id}> is the best")
     #await ctx.send(member.mention + ' is on the couch')
 
 @client.command(aliases = ['ping'], brief = 'Tells user their ping')
