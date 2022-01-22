ac88b17c0d1957cdd98d4792afcc22ba028d74f0
Kevin-Blimline <64049429+Kevin-Blimline@users.noreply.github.com>
2022-01-21 19:29:02 -0500

    Update kevbot.py

--- kevbot.py
+++ kevbot.py
@@ -22,8 +22,10 @@ async def RandomGame(ctx, *, games):
 @client.command(aliases = ['wheresmatt', 'Wheresmatt', 'WhereIsMatt', 'whereismatt'], brief = 'Tells user where Matt Crump is')
 async def WheresMatt(ctx):
     #member = discord.utils.get(ctx.message.guild.members, name='GoatMilk')
-    member = discord.utils.get(client.users, name="GoatMilk", discriminator="8908")
-    await ctx.send(member.mention + ' is on the couch')
+    #member = discord.utils.get(client.users, name="GoatMilk", discriminator="8908")
+    member = "304791011635494913"
+    await message.channel.send(f"<@{member}> is on the couch")
+    #await ctx.send(member.mention + ' is on the couch')
 
 @client.command(aliases = ['ping'], brief = 'Tells user their ping')
 async def Ping(ctx):
